2efc98cb4322a40771e1898bad0ea465baa00e7b
paulocoppe10 <paulocoppe10@gmail.com>
2022-04-29 11:48:33 -0300

    Layout page

---
+++ app/Views/principal.php
@@ -0,0 +1,33 @@
+
+    <style type="text/css">
+
+    #escrita{
+        text-align: justify;
+        width:800px;
+        margin-left: 250px;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+    #cor{
+    background-color: rgb(36, 36, 36);
+    color:white;
+    
+    }
+        </style>
+
+<div id="cor">
+        <h6 id="escrita">
+    - Já pensou estudar em uma escola onde você pode cursar programação ou criação de jogos, dentre outros cursos? E o melhor de tudo, com uma estrutura de laboratórios incríveis para aulas práticas do curso, e tudo isso gratuitamente. A Etec tem o objetivo de formar novos profissionais e capacitar nossos jovens para o mercado de trabalho. Se ficou interessado garanta já sua vaga no vestibular Etec 2022. Boa Sorte!
+    <p>
+    <p>
+    - A Etec é uma escola de ensino profissionalizante que pertence ao Centro Paula Souza, que fornece cursos gratuitos. Nela, há  duas formas de ensino, sendo eles o integrado, no qual você cursará o ensino médio juntamente com o técnico, e o técnico, no qual você cursará apenas o curso técnico escolhido.
+    <p>
+    - Integrado: é o ensino médio integrado ao técnico. Os cursos disponíveis são Técnico em Desenvolvimento de Sistemas, Administração e Programação de Jogos Digitais. Clique aqui e saiba mais.
+    <p>
+    - Técnico: é apenas o ensino técnico, sem o ensino médio. Os cursos disponíveis são Técnico em Administração, Técnico em Contabilidade, Técnico em Edificações e Técnico em Turismo Receptivo. Clique aqui e saiba mais.
+    <p>
+    - Para poder participar da Etec, é necessário fazer o Vestibulinho, mas o que é o Vestibulinho?
+    <p>
+    - O Vestibulinho é uma prova, que tem o intuito de classificar novos estudantes através de seus conhecimentos prévios acumulados, para assim poder definir quais serão os alunos que irão ingressar na instituição da Etec, sendo essa a Etec de Peruíbe<h6>
+
+</div>
---
+++ app/Views/template/footer.php
@@ -0,0 +1,19 @@
+<style type="text/css">
+    #direitos{
+        color: white;
+    }
+    </style>
+<div class="row">
+            <div class="col">
+                <h9 id="direitos" >todos os direitos reservados<h9>
+            </div>
+        </div> 
+    </div>
+    <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="http://etecperuibe.com.br/">Para mais informações!!!</a>
+  </li>
+  </li>
+</ul>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+
